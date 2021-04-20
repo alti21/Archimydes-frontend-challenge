b594caf1a895be052d887cf0427f195b7b1c8b8b
Altemush Bhatti <altemush@gmail.com>
2021-04-20 13:02:47 -0700

    feat: create login page

--- frontend-challenge/src/components/Login.js
+++ frontend-challenge/src/components/Login.js
@@ -4,7 +4,12 @@ const Login = () => {
 
     return (
         <div>
-            
+            <form className='login-form'>
+                <input type='email' name='email' placeholder='Email' />
+                <input type='password' name='password' placeholder='Password' />
+                <button>Login</button>
+                <button>Switch to {}</button>
+            </form>
         </div>
     )
 }
